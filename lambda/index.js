4cd6a71eaaa9620a33e329c0649a7e34342aee2e
Chris Feldmann <cfeldmann@gmail.com>
2017-11-21 16:36:53 -0600

    cacheControl headers

--- lambda/index.js
+++ lambda/index.js
@@ -3,29 +3,12 @@
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
 const Sharp = require('sharp');
-
 const BUCKET = process.env.BUCKET;
-
 const URL = process.env.URL;
-function iterate(obj) {
-    for (var property in obj) {
-        if (obj.hasOwnProperty(property)) {
-            if (typeof obj[property] == "object"){
-              console.log( 'ITERATING: ' + property );
-                iterate(obj[property]);
-            } else {
-              console.log('***' + property + "   " + obj[property]);              
-            }
-                
-        }
-    }
-}
 
 exports.handler = function(event, context, callback) {
 
-  // iterate( event );
   const key = event.queryStringParameters.key;
-  console.log( 'KEY: ' + key);
   const match = key.match(/([a-z0-9\-]+)\/(\d+)?x(\d+)?(.*)/);
   const prefix = match[1];
   const width = match[2] ? parseInt(match[2], 10) : null;  
@@ -34,10 +17,8 @@ exports.handler = function(event, context, callback) {
   const originalKey = prefix + '/full' + extension;
   console.log('bucket: ' + BUCKET);
   console.log( 'originalKey: ' + originalKey);
-
-
-
   const imageType = extension.split('.').pop();
+  const contentType = 'image/' + imageType; 
   
   const newKey = prefix + '/' + ((width === null) ? '' : width) + 'x' + ((height === null) ? '' : height) + '.' + imageType;
   console.log( 'newKey ', newKey);
@@ -53,6 +34,8 @@ exports.handler = function(event, context, callback) {
         Bucket: BUCKET,
         ContentType: 'image/' + imageType,
         Key: newKey,
+        // ACL : 'public-read',
+        CacheControl : 'public,max-age=31536000,immutable'
       }).promise()
     )
     .then(() => callback(null, {
